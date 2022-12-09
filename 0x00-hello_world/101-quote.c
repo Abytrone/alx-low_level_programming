7b4af0aeee86f7c8fbcddd4a2bb61664d050d8e9
Abytrone <abdulbasit.yahaya51@gmail.com>
2022-12-09 00:42:50 +0000

    A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print quote
  *
- * Return: Always 0 (Success)
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
