abdd97de6138b57e73a4db244cb1638732b943d0
Abytrone <38267924+Abytrone@users.noreply.github.com>
2022-12-10 21:03:12 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-		printf("\n");
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
 
 	return (0);
 }
