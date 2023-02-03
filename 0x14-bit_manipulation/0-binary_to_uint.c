3c0275381432e0f961a74218001cb2f54c3a8e1f
Abytrone <abdulbasit.yahaya51@gmail.com>
2023-02-03 01:50:29 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0;
-	int len, i;
-	int decval = 1;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	i = (len - 1);
-	for (; i >= 0; i--)
+	while (b[i + 1])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		if (b[i] == '1')
-			total += decval;
-		decval *= 2;
+		i++;
 	}
 
-	return (total);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
